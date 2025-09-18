63b91672fab64df313746db2cb8e1d2d9c2bd931
LinYingXiong <632162883@qq.com>
2025-09-19 00:33:57 +0800

    会话聊天功能完成

--- src/main/java/cn/itcast/demo/linyingxiongai2/config/CommonConfiguration.java
+++ src/main/java/cn/itcast/demo/linyingxiongai2/config/CommonConfiguration.java
@@ -24,8 +24,8 @@ public class CommonConfiguration {
                 .builder(model)
                 .defaultSystem("你是一个热心、可爱的智能助手，你的名字叫小团团，请以小团团的身份和语气回答问题。")
                 .defaultAdvisors(
-                        new SimpleLoggerAdvisor(),
-                        new MessageChatMemoryAdvisor(chatMemory)
+                        new SimpleLoggerAdvisor(), //日志功能
+                        new MessageChatMemoryAdvisor(chatMemory)  //会话记忆功能
                 )
                 .build();
     }
--- src/main/java/cn/itcast/demo/linyingxiongai2/controller/ChatController.java
+++ src/main/java/cn/itcast/demo/linyingxiongai2/controller/ChatController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
+import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY;
+
+/**
+ * 前端地址 http://localhost:5173/
+ */
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/ai")
@@ -14,11 +19,12 @@ public class ChatController {
 
     private final ChatClient chatClient;
 
-    //http://localhost:8083/ai/chat?prompt=你是谁
+    //http://localhost:8080/ai/chat?prompt=你是谁
     @RequestMapping(value = "/chat", produces = "text/html;charset=utf-8")
-    public Flux<String> chat(String prompt) {
+    public Flux<String> chat(String prompt,String chatId) {
         return chatClient.prompt()
                 .user(prompt)
+                .advisors(a -> a.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId)) //会话ID设置
                 .stream()
                 .content();
     }
