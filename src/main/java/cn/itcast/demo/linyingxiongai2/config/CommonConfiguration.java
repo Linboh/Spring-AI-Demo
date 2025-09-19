434eaeb611c4d0871652e7379eed1792d8e818a7
LinYingXiong <632162883@qq.com>
2025-09-19 15:24:33 +0800

    哄哄模拟器

--- src/main/java/cn/itcast/demo/linyingxiongai2/config/CommonConfiguration.java
+++ src/main/java/cn/itcast/demo/linyingxiongai2/config/CommonConfiguration.java
@@ -1,12 +1,14 @@
 package cn.itcast.demo.linyingxiongai2.config;
 
 
+import cn.itcast.demo.linyingxiongai2.constants.SystemConstants;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,6 +20,12 @@ public class CommonConfiguration {
         return new InMemoryChatMemory();
     }
 
+    /**
+     * 智能聊天机器人
+     * @param model
+     * @param chatMemory
+     * @return
+     */
     @Bean
     public ChatClient chatClient(OllamaChatModel model, ChatMemory chatMemory) {
         return ChatClient
@@ -26,6 +34,24 @@ public class CommonConfiguration {
                 .defaultAdvisors(
                         new SimpleLoggerAdvisor(), //日志功能
                         new MessageChatMemoryAdvisor(chatMemory)  //会话记忆功能
+                )
+                .build();
+    }
+
+    /**
+     * 哄哄模拟器
+     * @param model
+     * @param chatMemory
+     * @return
+     */
+    @Bean
+    public ChatClient gameChatClient(OpenAiChatModel model, ChatMemory chatMemory) {
+        return ChatClient
+                .builder(model)
+                .defaultSystem(SystemConstants.GAME_SYSTEM_PROMPT)
+                .defaultAdvisors(
+                        new SimpleLoggerAdvisor(),
+                        new MessageChatMemoryAdvisor(chatMemory)
                 )
                 .build();
     }
